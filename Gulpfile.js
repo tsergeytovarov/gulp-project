5967fb8229810ef911aff2748cfad8ca67730956
Сергей Попов <tovarov.piter@gmail.com>
2015-10-17 17:42:18 +0300

    Базовый шаблон на LESS

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,32 +1,155 @@
 /*
-  Author - Sergey Popov.
-  Author URI: http://ourworkspace.ru
-  Author social: https://vk.com/sergeytovarov
-  Author email: tovarov.piter@gmail.com
-  From Saint-Petersburg with love
+  Автор - Sergey Popov.
+  Сайт автора: http://ourworkspace.ru
+  Email: tovarov.piter@gmail.com
+  Из Санкт-Петербурга с любовью
 */
 
-// gulp init
-var gulp = require('gulp');
+'use strict';
 
-// plugins init
-var clean = require('gulp-clean'); // Clean paths
-var less = require('gulp-less'); // LESS Compiler
-var path = require('path');
+/* Подключение необходимых плагинов */
+var gulp = require('gulp'),
+    path = require('path'),
+    watch = require('gulp-watch'),
+    rename = require("gulp-rename"),
+    del = require('del'),
+    rigger = require('gulp-rigger'),
+    concat = require('gulp-concat'),
+    streamqueue = require('streamqueue'),
+    sourcemaps = require('gulp-sourcemaps'),
+    less = require('gulp-less'),
+    lessReporter = require('gulp-less-reporter'),
+    autoprefixer = require('gulp-autoprefixer'),
+    csscomb = require('gulp-csscomb'),
+    minifycss = require('gulp-minify-css'),
+    uglify = require('gulp-uglify'),
+    jshint = require('gulp-jshint'),
+    stylish = require('jshint-stylish'),
+    browserSync = require("browser-sync"),
+    reload = browserSync.reload,
+    runSequence = require('run-sequence').use(gulp);
 
-gulp.task('clean', function () {
-    return gulp.src('build', {read: true})
-        .pipe(clean());
+/* Настройка деррикторий */
+var projectPath = {
+  build: {
+    html: 'build/',
+    js: 'build/js/',
+    jsMainFile: 'script.js',
+    css: 'build/css/',
+    img: 'build/img/',
+    fonts: 'build/css/fonts/'
+  },
+  src: {
+    html: 'src/*.html',
+    jsCustom: 'src/js/custom.js',
+    jsVendor: 'src/js/vendor.js',
+    style: 'src/css/style.less',
+    formats: ['src/img/*.png', 'src/img/*.jpg', 'src/img/*.svg'],
+    fonts: 'src/css/fonts/*.*'
+  },
+  watch: {
+    html: 'src/*.html',
+    js: 'src/js/**/*.js',
+    style: 'src/css/**/*.less',
+    formats: ['src/img/*.png', 'src/img/*.jpg', 'src/img/*.svg'],
+    fonts: 'src/css/fonts/*.*'
+  },
+  clean: ['build/**/*', '!build/.gitignore']
+};
+
+/* Конфигурация BrowserSync */
+var config = {
+  server: {
+    baseDir: "./build"
+  },
+  tunnel: true,
+  host: 'localhost',
+  port: 9000,
+  injectChanges: true,
+  logPrefix: "gulp template"
+};
+
+/* BrowserSync*/
+gulp.task('webserver', function () {
+  browserSync(config);
 });
 
-// gulp less task
-gulp.task('less', function () {
-  return gulp.src('src/less/style.less')
+gulp.task('js', function () {
+  return streamqueue(
+    { objectMode: true },
+    gulp.src(projectPath.src.jsVendor).pipe(rigger()),
+    gulp.src(projectPath.src.jsCustom).pipe(rigger()).pipe(jshint()).pipe(jshint.reporter(stylish))
+  )
+  .pipe(concat(projectPath.build.jsMainFile))
+  .pipe(sourcemaps.init())
+  .pipe(gulp.dest(projectPath.build.js))
+  .pipe(rename({ suffix: '.min' }))
+  .pipe(uglify())
+  .pipe(sourcemaps.write('./'))
+  .pipe(gulp.dest(projectPath.build.js))
+  .pipe(reload({stream: true}));
+});
+
+gulp.task('html', function() {
+  return gulp.src(projectPath.src.html)
+    .pipe(gulp.dest(projectPath.build.html))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('less', function() {
+  return gulp.src(projectPath.src.style)
+    .pipe(sourcemaps.init())
     .pipe(less({
-      paths: [ path.join(__dirname, 'less', 'includes') ]
+        paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
-    .pipe(gulp.dest('build/css/'));
+    .on('error', lessReporter)
+    .pipe(autoprefixer('> 2%'))
+    .pipe(csscomb())
+    .pipe(gulp.dest(projectPath.build.css))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(minifycss())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(projectPath.build.css))
+    .pipe(reload({stream: true}));
 });
 
-// base gulp task
-gulp.task('default', ['clean','less']);
+gulp.task('images', function () {
+  return gulp.src(projectPath.src.formats)
+    .pipe(gulp.dest(projectPath.build.img))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('fonts', function() {
+  return gulp.src(projectPath.src.fonts)
+    .pipe(gulp.dest(projectPath.build.fonts))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('clean', function (cb) {
+  del(projectPath.clean, cb);
+});
+
+gulp.task('build', function(callback) {
+  runSequence('clean','html','fonts','less','images','js',callback)
+});
+
+gulp.task('watch',['webserver'], function(){
+  watch([projectPath.watch.js], function(event, cb) {
+    gulp.start('js');
+  });
+  watch([projectPath.watch.style], function(event, cb) {
+    gulp.start('less');
+  });
+  watch([projectPath.watch.formats], function(event, cb) {
+    gulp.start('images');
+  });
+  watch([projectPath.watch.fonts], function(event, cb) {
+    gulp.start('fonts');
+  });
+  watch([projectPath.watch.html], function(event, cb) {
+    gulp.start('html');
+  });
+});
+
+/* Базовый таск */
+gulp.task('default', ['watch'], function() {});
---
+++ License.md
@@ -0,0 +1,13 @@
+# The MIT License
+
+Copyright © 2015 Sergey Popov, http://ourworkspace.ru/about
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so.
+
+---
+
+# Лицензия MIT
+
+Copyright © 2015 Sergey Popov, http://ourworkspace.ru/about
+
+Данная лицензия разрешает лицам, получившим копию данного программного обеспечения и сопутствующей документации (в дальнейшем именуемыми «Программное Обеспечение»), безвозмездно использовать Программное Обеспечение без ограничений, включая неограниченное право на использование, копирование, изменение, добавление, публикацию, распространение, сублицензирование и/или продажу копий Программного Обеспечения, также как и лицам, которым предоставляется данное Программное Обеспечение.
